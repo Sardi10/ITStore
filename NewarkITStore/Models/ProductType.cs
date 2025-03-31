b3e6269b1639d4591955777783f189559e874d4e
sardi <sardi.kuka@fshnstudent.info>
2025-03-30 22:23:49 -0400

    Added models: BasketItem,Product and ProductType

--- NewarkITStore/Models/ProductType.cs
+++ NewarkITStore/Models/ProductType.cs
@@ -1,6 +1,13 @@
-﻿namespace NewarkITStore.Models
+﻿public class ProductType
 {
-    public class ProductType
-    {
-    }
+    public int ProductTypeId { get; set; }
+    public string Name { get; set; } // e.g., Laptop, Desktop, Printer, Accessory
+
+    public string? CPU { get; set; }
+    public float? Weight { get; set; }
+    public float? BatteryLife { get; set; }
+    public string? Resolution { get; set; }
+    public string? PrinterType { get; set; }
+
+    public ICollection<Product> Products { get; set; }
 }

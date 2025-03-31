e59a55367415e982c368bfb9215dc5c0e69967b0
sardi <sardi.kuka@fshnstudent.info>
2025-03-30 23:02:38 -0400

    Modified Home Controller to show products

--- NewarkITStore/Controllers/HomeController.cs
+++ NewarkITStore/Controllers/HomeController.cs
@@ -1,22 +1,32 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using NewarkITStore.Models;
+using NewarkITStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace NewarkITStore.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var products = await _context.Products
+            .Include(p => p.ProductType)
+            .ToListAsync();
+
+        return View(products);
     }
+
 
     public IActionResult Privacy()
     {

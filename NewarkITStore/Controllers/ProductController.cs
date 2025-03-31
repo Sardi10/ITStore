1644ceeac034ff6d0cef811b18be0dac73c5be6c
sardi <sardi.kuka@fshnstudent.info>
2025-03-31 00:00:14 -0400

    Added create and edit functionalities and views for products

--- NewarkITStore/Controllers/ProductController.cs
+++ NewarkITStore/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using NewarkITStore.Data;
 using NewarkITStore.Models;
@@ -30,5 +31,74 @@ namespace NewarkITStore.Controllers
 
             return View(products);
         }
+
+        // GET: /Product/Create
+        [Authorize(Roles = "Admin")]
+        public IActionResult Create()
+        {
+            ViewBag.ProductTypes = new SelectList(_context.ProductTypes, "ProductTypeId", "Name");
+            return View();
+        }
+
+        // POST: /Product/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Manage");
+            }
+
+            ViewBag.ProductTypes = new SelectList(_context.ProductTypes, "ProductTypeId", "Name", product.ProductTypeId);
+            return View(product);
+        }
+
+        // GET: /Product/Edit/5
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null) return NotFound();
+
+            ViewBag.ProductTypes = new SelectList(_context.ProductTypes, "ProductTypeId", "Name", product.ProductTypeId);
+            return View(product);
+        }
+
+        // POST: /Product/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(int id, Product product)
+        {
+            if (id != product.ProductId) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Manage");
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Products.Any(p => p.ProductId == id))
+                        return NotFound();
+                    else
+                        throw;
+                }
+            }
+
+            ViewBag.ProductTypes = new SelectList(_context.ProductTypes, "ProductTypeId", "Name", product.ProductTypeId);
+            return View(product);
+        }
+
+
     }
 }

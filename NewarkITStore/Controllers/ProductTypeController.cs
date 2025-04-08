b0052b912c561dbd4c05189987833566b3380cfc
sardi <sardi.kuka@fshnstudent.info>
2025-04-08 00:19:10 -0400

    Added ProductType Controller

---
+++ NewarkITStore/Controllers/ProductTypeController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NewarkITStore.Data;
+using NewarkITStore.Models;
+
+namespace NewarkITStore.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class ProductTypeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductTypeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ProductTypes.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductType productType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(productType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productType);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var type = await _context.ProductTypes.FindAsync(id);
+            if (type == null) return NotFound();
+
+            return View(type);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ProductType productType)
+        {
+            if (id != productType.ProductTypeId) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(productType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var type = await _context.ProductTypes.FindAsync(id);
+            if (type != null)
+            {
+                _context.ProductTypes.Remove(type);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}

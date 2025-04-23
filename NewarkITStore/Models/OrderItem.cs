15d86598ec8187ba624a92af34eb79e69eb2eb20
sardi10 <sardikuka@gmail.com>
2025-04-23 05:37:36 -0400

    Added Order and OrderItem to Models

---
+++ NewarkITStore/Models/OrderItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NewarkITStore.Models
+{
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+
+        public int OrderId { get; set; }
+        [ForeignKey("OrderId")]
+        public Order Order { get; set; }
+
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal PricePerUnit { get; set; }
+    }
+}

838fc27ab8375f13d5f2c35ee473ce91674a2db9
sardi10 <sardikuka@gmail.com>
2025-04-24 22:37:51 -0400

    Added credit card model

---
+++ NewarkITStore/Models/CreditCard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NewarkITStore.Models
+{
+    public class CreditCard
+    {
+        public int CreditCardId { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{16}$", ErrorMessage = "Card number must be exactly 16 digits.")]
+        [Display(Name = "Card Number")]
+        public string CardNumber { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{3,4}$", ErrorMessage = "CVV must be 3 or 4 digits.")]
+        [Display(Name = "Security Code (CVV)")]
+        public string SecurityCode { get; set; }
+
+        [Required]
+        [Display(Name = "Name on Card")]
+        public string CardHolderName { get; set; }
+
+        [Required]
+        [Display(Name = "Billing Address")]
+        public string BillingAddress { get; set; }
+
+        [Required]
+        [Display(Name = "Card Type")]
+        public string CardType { get; set; } // Visa, MasterCard, etc.
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Expiry Date")]
+        public DateTime ExpiryDate { get; set; }
+
+        // Foreign Key to ApplicationUser
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+    }
+}

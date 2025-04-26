fe71122a07984940ff02760428b6ff654b5c982c
sardi10 <sardikuka@gmail.com>
2025-04-26 08:05:25 -0400

    Added Checkout View Model

---
+++ NewarkITStore/ViewModels/CheckoutViewModel.cs
@@ -0,0 +1,7 @@
+﻿using NewarkITStore.Models;
+
+public class CheckoutViewModel
+{
+    public List<BasketItem> BasketItems { get; set; }
+    public List<ShippingAddress> ShippingAddresses { get; set; }
+}

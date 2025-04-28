95ee0830fd3cfcc9048bb5af4fce8cb55a561794
sardi10 <sardikuka@gmail.com>
2025-04-28 07:58:16 -0400

    Added exporting options

--- NewarkITStore/ViewModels/CreditCardStatsViewModel.cs
+++ NewarkITStore/ViewModels/CreditCardStatsViewModel.cs
@@ -5,5 +5,9 @@
         public string UserEmail { get; set; }
         public string MaskedCardNumber { get; set; }
         public decimal TotalCharged { get; set; }
+        public List<CreditCardStatsViewModel> Data { get; set; } = new();
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
     }
+
 }

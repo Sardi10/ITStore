6873b11b0a7ed5c2f5aef4e284fad4294e1a5b6c
sardi10 <sardikuka@gmail.com>
2025-04-28 18:50:40 -0400

    Added TopProductByCustomersViewModel

---
+++ NewarkITStore/ViewModels/Class.cs
@@ -0,0 +1,9 @@
+﻿namespace NewarkITStore.ViewModels
+{
+    public class TopProductByCustomersViewModel
+    {
+        public string ProductName { get; set; }
+        public int UniqueCustomerCount { get; set; }
+    }
+
+}
